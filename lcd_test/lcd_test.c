f6534cd535e1a81197308a60f29d99c00aabba0d
Pranjal <pranjalgupta1901@gmail.com>
2024-04-18 03:04:13 -0600

    adding lcd-testing code

---
+++ lcd_test/lcd_test.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include "lcd_display.h"
+
+int main() {
+    // Initialize LCD
+    lcd_init();
+
+    // Clear LCD
+    lcd_clear();
+
+    // Display a message
+    lcdputs("Hello, World!");
+
+    // Delay to observe the display
+    sleep(5);
+
+    // Clear LCD
+    lcd_clear();
+
+    // Display another message
+    lcdputs("Testing LCD driver!");
+
+    // Delay to observe the display
+    sleep(5);
+
+    // Clear LCD
+    lcd_clear();
+
+    // You can add more tests or functionality here
+
+    return 0;
+}
+
