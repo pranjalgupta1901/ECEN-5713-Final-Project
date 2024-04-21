821b80e6df59e9f7abdd1c3c4ee1de0167ba52d6
Pranjal Gupta <pranjalgupta1901@gmail.com>
2024-04-21 05:35:30 -0600

    LCD working and tested

--- lcd_display/lcd_display.h
+++ lcd_display/lcd_display.h
@@ -1,12 +1,9 @@
 #include <stdint.h>
 
 void lcd_init(void);
-void send_data_or_command(unsigned char ch);
-void delay(void);
-void lcdputch(unsigned char c);
 void lcd_clear(void);
-void lcdgotoaddr(uint8_t address);
 void lcdgotoxy(uint8_t set_row, uint8_t set_column);
-void lcdputs(const char *str);
-void open_i2c_file();
-void close_i2c_file();
+void lcdputch(unsigned char c);
+void lcdputs(char *str);
+void lcd_move_and_write(int x, int y, char *data);
+void close_i2c_file() ;
